263ca88ca2bed3540ad6f6bcda1a06b9e9c150be
Tom Vinnicombe <tom_vinnicombe@live.com.au>
2017-05-11 21:25:16 +1000

    Styled about component and main component with foundation.

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to examples page!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to="/?location=Highett,Australia">Highett, AUS</Link>
+        </li>
+        <li>
+          <Link to="/?location=Brisbane,Australia">Brisbane, AUS</Link>
+        </li>
+      </ol>
     </div>
   )
 };
