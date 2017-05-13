17c6546b5c15c6127095db6861d39e0b783d56de
Tom Vinnicombe <tom_vinnicombe@live.com.au>
2017-05-13 13:45:05 +1000

    Updated and styled about page. Linked to resources used.

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,14 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page.</p>
+      <h1 className="text-center">About</h1>
+      <p>This is a simple single page weather application. It's built with React.js
+         and uses the Open Weather Map API. </p>
+      <p>Here are some of the tools I used:</p>
+      <ul>
+        <li><a href="https://facebook.github.io/react/">React.JS</a></li>
+        <li><a href="https://openweathermap.org/">Open Weather Map API</a></li>
+      </ul>
     </div>
   )
 };
