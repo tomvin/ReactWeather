90b1693d13a0cb8c2bedc174c64b61d294c91a2f
Tom Vinnicombe <tom_vinnicombe@live.com.au>
2017-05-13 20:15:42 +1000

    Implemented the search in the nav bar. Changed the Weather component to use query strings to search automatically if a location is passed through the URL.

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,7 +4,13 @@ var {Link, IndexLink} = require('react-router');
 var Nav = React.createClass({
   onSearch: function (e) {
       e.preventDefault();
-      alert('Not yet wired up!');
+      var location = this.refs.location.value;
+
+      if(location.length > 0) {
+        var encodedLocation = encodeURIComponent(location);
+        this.refs.location.value = '';
+        window.location.hash = '#/?location=' + encodedLocation;
+      }
   },
   render: function () {
     return (
@@ -27,7 +33,7 @@ var Nav = React.createClass({
           <form onSubmit={this.onSearch}>
             <ul className="menu">
               <li>
-                <input type="search" placeholder="Search weather by city"/>
+                <input type="search" ref="location" placeholder="Search weather by city"/>
               </li>
               <li>
                 <input type="submit" className="button" value="Get Weather"/>
